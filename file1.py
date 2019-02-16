05a9ea4c2ab9d5a56fee4f6dd21314720973a2e2
ghjuliasialelli <45049580+ghjuliasialelli@users.noreply.github.com>
2019-02-16 10:13:08 +0100

    Distance matrices

--- file1.py
+++ file1.py
@@ -21,55 +21,54 @@ origins = urllib.parse.quote_plus(locations[0])
 for i in range(1, len(locations)):
     origins = origins+"|"+urllib.parse.quote_plus(locations[i])
 
-# Step 1 : construct the URL for driving
-# example : https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&key=YOUR_API_KEY
-params_driving = {'origins':origins, 'destinations':origins, 'key':API_KEY}
-        #base_url_driving = base_url + "origins=" + origins + "&destinations=" + origins + "&key=" + API_KEY
-r_driving = requests.get(base_url, params=params_driving)
-data_driving = r_driving.text
 
-# Step 2 : construct the URL for transit 
-params_transit = {'origins':origins, 'destinations':origins, 'mode':'transit', 'key':API_KEY}
-        #base_url_driving = base_url + "origins=" + origins + "&destinations=" + origins + "mode=transit" + "&key=" + API_KEY
-r_transit = requests.get(base_url, params=params_transit)
-data_transit = r_transit.text
+def distance_matrix(voiture_dispo):
+    matrix_transit = matrix_transit()
+    res = [matrix_transit]
+    if voiture_dispo : 
+        matrix_driving = matrix_driving()
+        res.append(matrix_driving)
+    return res 
+    
+    
+def matrix_driving():
+    params_driving = {'origins':origins, 'destinations':origins, 'key':API_KEY}
+    r_driving = requests.get(base_url, params=params_driving)
+    data_driving = r_driving.text
 
+    # Parse input XML for driving 
+    root_driving = PARSER.fromstring(data_driving)
+    driving_matrix = [[0 for _ in range(len(locations))] for _ in range(len(locations))]
 
-# Parse input XML for driving 
-root_driving = PARSER.fromstring(data_driving)
-driving_matrix = [[0 for _ in range(len(locations))] for _ in range(len(locations))]
+    i=0
+    for row in root_driving.getiterator("row"):
+        j = 0
+        for column in row.getiterator("element"):
+            for duration in column.getiterator("duration"):
+                for value in list(duration):
+                    if value.tag == "value": 
+                        print("i,j", (i,j))
+                        driving_matrix[i][j] = int(value.text)/60
+            j+=1
+        i+=1
 
-print(data_driving)
+def matrix_transit():
+    params_transit = {'origins':origins, 'destinations':origins, 'mode':'transit', 'key':API_KEY}
+    r_transit = requests.get(base_url, params=params_transit)
+    data_transit = r_transit.text
 
-print("len location", len(locations))
+    # Parse input XML for transit 
+    root_transit = PARSER.fromstring(data_transit)
+    transit_matrix = [[0 for _ in range(len(locations))] for _ in range(len(locations))]
 
-i=0
-for row in root_driving.getiterator("row"):
-    j = 0
-    for column in row.getiterator("element"):
-        for duration in column.getiterator("duration"):
-            for value in list(duration):
-                if value.tag == "value": 
-                    print("i,j", (i,j))
-                    driving_matrix[i][j] = value.text 
-        j+=1
-    i+=1
+    i=0
+    for row in root_transit.getiterator("row"):
+        j=0
+        for column in row.getiterator("element"):
+            for duration in column.getiterator("duration"):
+                for value in list(duration):
+                    if value.tag == "value": 
+                        transit_matrix[i][j] = int(value.text)/60
+            j+=1
+        i+=1
 
-# Parse input XML for transit 
-root_transit = PARSER.fromstring(data_transit)
-transit_matrix = [[0 for _ in range(len(locations))] for _ in range(len(locations))]
-
-i=0
-for row in root_transit.getiterator("row"):
-    j=0
-    for column in row.getiterator("element"):
-        for duration in column.getiterator("duration"):
-            for value in list(duration):
-                if value.tag == "value": 
-                    transit_matrix[i][j] = value.text 
-        j+=1
-    i+=1
-
-
-print(driving_matrix)
-print(transit_matrix)
