aeff8255b65bbffd3a4087a838671d5323d4eeda
ghjuliasialelli <45049580+ghjuliasialelli@users.noreply.github.com>
2019-02-16 10:57:57 +0100

    Add files via upload

---
+++ sample_input.py
@@ -0,0 +1,7 @@
+inputt =    [[225,"Grand Hôtel d'Aboukir", "134 rue d'Aboukir","75002", 35, 7], 
+            [7180332, "Chaussée d'Antin", "46 rue de la Chaussée d'Antin", "75009", 34, 3]
+            [3029096, "Métropolitain", "158 rue Oberkampf", "75011", 60, 5]
+            [69, "Cristal","64 rue de la Jonquière","75017", 98, 10]
+            [160, "des Beaux-Arts","4 rue André Antoine","75018", 76, 2]
+            [14664544, "Moderne - Paris 20ème","57 rue de la Réunion","75020", 41,10]] 
+
